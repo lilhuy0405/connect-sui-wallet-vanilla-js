1430fccdbd3bd22e11749a086e44cc100dec05fc
lilhuy0405 <luuhuy040501.travail@gmail.com>
2023-05-04 16:04:07 +0700

    injected suiWallet to window obj

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,5 @@
 import { WalletStandardAdapterProvider } from "@mysten/wallet-adapter-all-wallets";
 
-//static html elements
-const connectButton = document.getElementById("connectButton");
-const connectStatusRes = document.getElementById("connected-status");
-const SUI_WALLET = "Sui Wallet";
-if (connectButton) {
-  connectButton.addEventListener("click", connect);
-}
 /*
   Connect wallet steps:
   1. Get all available wallets
@@ -15,23 +8,33 @@ if (connectButton) {
   4. Get address
   5. Listen to change event 
 */
+const SUI_WALLET = "Sui Wallet";
+const allWallets = new WalletStandardAdapterProvider().get(); //get all available wallets
+//for quick demo we only find sui wallet then connect to it 
+const suiWallet = allWallets.find((wallet) => wallet.name === SUI_WALLET);
+window.suiWallet = suiWallet;
+
+//static html elements
+const connectButton = document.getElementById("connectButton");
+const connectStatusRes = document.getElementById("connected-status");
+
+if (connectButton) {
+  connectButton.addEventListener("click", connect);
+}
+
 async function connect() {
   try {
-    const allWallets = new WalletStandardAdapterProvider().get(); //get all available wallets
-    //for quick demo we only find sui wallet then connect to it 
-    const suiWallet = allWallets.find((wallet) => wallet.name === SUI_WALLET);
-    window.suiWallet = suiWallet;
-    if (!suiWallet) {
+    if (!window.suiWallet) {
       alert("Sui Wallet not installed");
       return;
     }
-    if (!suiWallet.connected) {
-      await suiWallet.connect();
+    if (!window.suiWallet.connected) {
+      await window.suiWallet.connect();
     }
-    console.log(suiWallet);
+    console.log(window.suiWallet);
 
     //get address 
-    const accounts = await suiWallet.getAccounts();
+    const accounts = await window.suiWallet.getAccounts();
     console.log(accounts);
 
     if (accounts.length == 0) {
@@ -39,12 +42,12 @@ async function connect() {
       return;
     }
 
-    suiWallet.on("change", ({ connected, accounts }) => {
+    window.suiWallet.on("change", ({ connected, accounts }) => {
       const newAccount = accounts[0];
       if (connected) {
         connectStatusRes.innerHTML = `
     <p>Connect to ${SUI_WALLET}</p>
-    <p>icon: <img src="${suiWallet.icon}"/></p>
+    <p>icon: <img src="${window.suiWallet.icon}"/></p>
     <p>Chain: ${newAccount.chains[0]}</p>
     <p>Address: ${newAccount.address}</p>
   `;
@@ -56,7 +59,7 @@ async function connect() {
     const account = accounts[0];
     connectStatusRes.innerHTML = `
     <p>Connect to ${SUI_WALLET}</p>
-    <p>icon: <img src="${suiWallet.icon}"/></p>
+    <p>icon: <img src="${window.suiWallet.icon}"/></p>
     <p>Chain: ${account.chains[0]}</p>
     <p>Address: ${account.address}</p>
   `;
